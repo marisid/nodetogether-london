ff23473b8e90abe7c78ca1b0402c497354256367
marisid <sid_marina@hotmail.com>
2016-07-20 21:32:39 +0100

    add random message to server response

--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
 var Express = require('express');
 var app = Express();
+var PickOne = require('pick-one');
+var Messages =  ['hi','world','universe','galaxy'];
 
 app.get('/hello', function(req,res) {
 	console.log("request to /hello");
+	res.send(pickOne(Messages));
+});
+
+/*app.get('*', function(req,res) {
+	console.log("request to /hello");
 	res.send("Hello!");
-})
+});*/
+
 //to "expose" the app so that server.js can use it
 module.exports = app;
