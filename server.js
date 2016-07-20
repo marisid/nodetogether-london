719b81fafc00414bf54097500a239444ee692e08
marisid <sid_marina@hotmail.com>
2016-07-20 21:19:25 +0100

    update to support heroku port and host

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // for an npm package to work we need to save it in a variable
 
-var port = "8000";
-var host = "0.0.0.0";
+var port = process.env.PORT || "8000";
+var host = process.env.HOST || "0.0.0.0";
 var Express = require('express');
 var rules = require('./app/index.js')
 
